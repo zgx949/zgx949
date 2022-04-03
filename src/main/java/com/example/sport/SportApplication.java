2852b3f3ef1bbc70a142cece4b7c42d8445705ce
lyhsy <77bz@163.com>
2022-04-03 20:49:42 +0800

    王总到此一游，，，

--- src/main/java/com/example/sport/SportApplication.java
+++ src/main/java/com/example/sport/SportApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan("com.example.sport.Mapper")
 public class SportApplication {
-
+    /**
+     * 小王同学到此一游。。。。。
+     * @param args
+     */
     public static void main(String[] args) {
         SpringApplication.run(SportApplication.class, args);
     }
