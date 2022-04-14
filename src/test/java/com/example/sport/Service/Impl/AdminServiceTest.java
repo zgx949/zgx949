d0b5c57654ff5641574e13d20e1f669520a39278
zgx <1637349696@qq.com>
2022-04-14 13:09:02 +0800

    完成AdminService所有的方法测试

--- src/test/java/com/example/sport/Service/Impl/AdminServiceTest.java
+++ src/test/java/com/example/sport/Service/Impl/AdminServiceTest.java
@@ -1,7 +1,7 @@
 package com.example.sport.Service.Impl;
 
+import com.example.sport.Bean.AdminBean;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
@@ -25,6 +25,56 @@ public class AdminServiceTest {
             System.out.println("账号密码错误");
         }
         System.out.println(result);
+    }
+
+    @Test
+    public void testGetMenu() {
+        // TODO: 未测试线程安全情况
+        for (int i = 0; i < 5; i++) {
+            System.out.println(adminService.getMenu(i));
+        }
+    }
+
+    @Test
+    public void testExists() {
+        System.out.println(adminService.existsAdmin("admin"));
+    }
+
+    @Test
+    public void testInsertAdmin() {
+        AdminBean admin = new AdminBean("admin", "admin", "测试管理员", 0, 0);
+        System.out.println(adminService.insertAdmin(admin));
+    }
+
+    @Test
+    public void testUpdateAdmin() {
+        AdminBean admin = new AdminBean("admin", "adminupdate", "测试修改管理员", 0, 0);
+        System.out.println();
+    }
+
+    @Test
+    public void testDeleteAdmin() {
+        System.out.println(adminService.deleteAdmin(10));
+    }
+
+    @Test
+    public void testGetAdmin() {
+        System.out.println(adminService.getAdmin(1, 10));
+    }
+
+    @Test
+    public void testCountAdmin() {
+        System.out.println(adminService.countAdmin());
+    }
+
+    @Test
+    public void testAdminOptions() {
+        System.out.println(adminService.adminOptions());
+    }
+
+    @Test
+    public void testFindAdminById() {
+        System.out.println(adminService.findAdminById(1));
     }
 
 }
