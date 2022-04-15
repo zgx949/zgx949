ca29569038cb9950fa37c725b14c7b9479af7d2b
lyhsy <77bz@163.com>
2022-04-15 17:37:22 +0800

    完善了CollegesService的所有测试

---
+++ src/test/java/com/example/sport/Service/Impl/CollegesServiceTest.java
@@ -0,0 +1,50 @@
+package com.example.sport.Service.Impl;
+import com.example.sport.Bean.CollegesBean;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import javax.annotation.Resource;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author 小王同学
+ * @version 1.0
+ * @date 2022/4/15 16:53
+ */
+@SpringBootTest
+public class CollegesServiceTest {
+    @Resource
+    CollegesServiceImpl collegesService;
+
+    @Test
+    public void testInsertCollege() {
+        CollegesBean college = new CollegesBean(1,"操场",30);
+        System.out.println(collegesService.insertCollege(college));
+    }
+
+    @Test
+    public void testDelCollege() {
+        System.out.println(collegesService.delCollege(2));
+    }
+
+    @Test
+    public void testUpdateField() {
+        CollegesBean college = new CollegesBean(1,"体育馆",60);
+        System.out.println(collegesService.updateField(college));
+    }
+
+    @Test
+    public void testGetCollege() {
+        System.out.println(collegesService.getCollege(1,10));
+    }
+
+    @Test
+    public void testCountColleges() {
+        System.out.println(collegesService.countColleges());
+    }
+
+    @Test
+    public void testCollegesOptions() {
+        System.out.println(collegesService.collegesOptions());
+    }
+}
