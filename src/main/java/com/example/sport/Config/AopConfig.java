ea6459fa1b449bb3b596ff9fd05bd11bf1d15a60
guoxiang <1637349696@qq.com>
2024-05-16 23:47:09 +0800

    docx: delete info

--- src/main/java/com/example/sport/Config/AopConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.sport.Config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-
-@Configuration
-@EnableAspectJAutoProxy
-public class AopConfig {
-}
